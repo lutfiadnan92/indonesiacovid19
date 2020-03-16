2ecd665623d87afe2d8871127df98b918df58a27
lutfiadnan92 <adnanlutfi92@gmail.com>
2020-03-16 15:14:21 +0700

    update service-wroker

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,9 @@
 const cacheName = 'indonesia covid-19';
 const filesToCache = [
     '/',
-    'index.html',
+    '/index.html',
     '/src/assets/bootstrap.min.css',
     '/src/assets/style.css',
-    '/src/assets/jquery.js',
     'https://cdn.jsdelivr.net/npm/vue/dist/vue.js',
     'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js',
     '/src/index.js',
@@ -12,7 +11,7 @@ const filesToCache = [
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheName).then(cache => {
             return cache.addAll(filesToCache);
@@ -20,7 +19,7 @@ self.addEventListener('install', (e) => {
     );
 });
 /* Serve cached content when offline */
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
     e.respondWith(caches.match(e.request).then(res => {
             return res || fetch(e.request);
         })
