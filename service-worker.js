1528a0e79f9f349c167febdb68b1cf5b1c9b378f
lutfiadnan92 <adnanlutfi92@gmail.com>
2020-03-16 20:59:56 +0700

    create new function

--- service-worker.js
+++ service-worker.js
@@ -13,15 +13,18 @@ const filesToCache = [
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
     e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
+        caches.open(cacheName)
+            .then(function(cache) {
+                return cache.addAll(filesToCache);
         })
     );
 });
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-    e.respondWith(caches.match(e.request).then(function(res) {
-            return res || fetch(e.request);
+    e.respondWith(
+        caches.match(e.request)
+            .then(function(res) {
+                return res || fetch(e.request);
         })
     );
 });
--- src/index.js
+++ src/index.js
@@ -8,19 +8,35 @@ const app = new Vue({
             terkonfirmasi: 0,
             sembuh: 0,
             meninggal: 0,
-            covidData: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-15-2020.csv'
+            covidData: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/',
+            path: ''
         }
     },
     created() {
         this.$nextTick(() => {
-            this.loadData()
+            this.loadPath()
+            setTimeout(() => {
+                this.loadData()
+            }, 3000);
         })
     },
     methods: {
-        async loadData() {
+        async loadPath() {
             await axios({
                 method: 'get',
-                url: this.covidData,
+                url: 'https://api.github.com/repos/CSSEGISandData/COVID-19/git/trees/e22872e7e9ea17b968386c79437a431ebec09d7d'
+            }).then(res => {
+                res.data.tree.shift(); res.data.tree.pop();
+                const path = res.data.tree.splice(-1)
+                this.path = path[0].path
+            }).catch(err => {
+                console.log(err)
+            })
+        },
+        loadData() {
+            axios({
+                method: 'get',
+                url: this.covidData + this.path,
                 responseType: 'text'
             }).then(res => {
                 const array = this.csvToJson(res.data)
