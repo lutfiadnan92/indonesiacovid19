75a8c26faf61fd7f8e49db8ee3e725a97082eaf8
lutfiadnan92 <adnanlutfi92@gmail.com>
2020-03-18 08:16:09 +0700

    automatic update data every day

--- src/index.js
+++ src/index.js
@@ -1,57 +1,35 @@
 'use strict'
 
-const app = new Vue({
+new Vue({
     el: '#app',
     data() {
         return {
             date: '',
-            terkonfirmasi: 0,
-            sembuh: 0,
-            meninggal: 0,
-            covidData: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/',
-            path: ''
+            dataArr: [
+                { id: 1, cardStyle: 'border-warning', textStyle: 'text-warning', text: 'Terkonfirmasi', value: 0},
+                { id: 2, cardStyle: 'border-danger', textStyle: 'text-danger', text: 'Meninggal', value: 0 },
+                { id: 3, cardStyle: 'border-success', textStyle: 'text-success', text: 'Sembuh', value: 0 }
+            ],
+            github: {
+                user: 'CSSEGISandData/',
+                repo: 'COVID-19/',
+                repoLink: 'https://api.github.com/repos/',
+                rawLink: 'https://raw.githubusercontent.com/',
+            },
+            loading: {
+                interval: {},
+                value: 0,
+                status: false,
+                style: { height: '5px' }
+            },
         }
     },
-    created() {
+    mounted() {
         this.$nextTick(() => {
-            this.loadPath()
-            setTimeout(() => {
-                this.loadData()
-            }, 1000);
+            this.loadData()
         })
     },
     methods: {
-        async loadPath() {
-            await axios({
-                method: 'get',
-                url: 'https://api.github.com/repos/CSSEGISandData/COVID-19/git/trees/68141a0e97eab1c27130c75810d06f36e0ac925c'
-            }).then(res => {
-                res.data.tree.shift(); res.data.tree.pop();
-                const path = res.data.tree.splice(-1)
-                this.path = path[0].path
-            }).catch(err => {
-                console.log(err)
-            })
-        },
-        loadData() {
-            axios({
-                method: 'get',
-                url: this.covidData + this.path,
-                responseType: 'text'
-            }).then(res => {
-                const array = this.csvToJson(res.data)
-                return array.filter((value,index,array) => {
-                    if(array[index].CountryRegion === 'Indonesia'){
-                        this.date = value.LastUpdate.split('T')[0]
-                        this.terkonfirmasi = parseInt(value.Confirmed)
-                        this.sembuh = parseInt(value.Recovered)
-                        this.meninggal = parseInt(value.Deaths)
-                    }
-                })
-            }).catch(err => {
-                console.log(err)
-            })
-        },
         csvToJson(csv) {
             const lines = csv.split('\n')
             const result = []
@@ -67,6 +45,49 @@ const app = new Vue({
             })
             result.pop()
             return result
+        },
+        loadData() {
+            const vm = this
+            // get master branch
+            axios.get(vm.github.repoLink + vm.github.user + vm.github.repo + 'branches/master')
+            .then(res => {
+                // get commit sha
+                axios.get(vm.github.repoLink + vm.github.user + vm.github.repo +'commits/' + res.data.commit.sha)
+                .then(res => {
+                    const filesArr = res.data
+                    vm.date = res.data.commit.author.date.split('T')[0]
+                    return filesArr.files.filter((v,i,a) => {
+                        axios.get(vm.github.rawLink + vm.github.user + vm.github.repo + filesArr.sha + '/' + a[i].filename, { responseType: 'text' })
+                        .then(res => {
+                            const dataArr = vm.csvToJson(res.data)
+                            return dataArr.filter((a,b,c) => {
+                                if(c[b].CountryRegion === 'Indonesia') {
+                                    const { ProvinceState,CountryRegion,Lat,Long, ...obj} = a
+                                    vm.dataArr[i].value = Object.values(obj)[Object.values(obj).length - 1]
+                                }
+                            })
+                        })
+                    })
+                })
+            }).catch(err => {
+                console.log(err)
+            })
+        },
+        refresh() {
+            const vm = this
+            vm.loading.interval = setInterval(() => {
+                if (vm.loading.value === 100) {
+                    vm.loading.status = false
+                    clearInterval(vm.loading.interval)
+                    vm.loadData()
+                    return (vm.loading.value = 0)
+                }
+                vm.loading.status = true
+                vm.loading.value += 25
+                return vm.dataArr.filter((v,i,a) => {
+                    return a[i].value = 0
+                })
+            }, 300)
         }
     }
 })
